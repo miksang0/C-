a3aa7aeea2f2d915d1e19813d301d10a153c5102
Sujan Tamang <69522312+miksang0@users.noreply.github.com>
2024-08-29 13:32:51 -0500

    Add files via upload
    ahahaa, so our professor wanted to know about us, this is the very first assignment. A short intro to myself.

---
+++ WhoAmI.cpp
@@ -0,0 +1,30 @@
+//**********************************************************
+//* Sujan Tamang							               * 
+//* COSC 1550.07 Computer Programming I		               * 
+//* Program Name:	WhoAmI.cpp					           * 
+//* Homework Number 1, Assignment 1                        *
+//* Problem #: 1                                           *
+//* Due Date: Aug 29 at 5:30p 				   			   * 
+//**********************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "My name is Sujan Tamang." << endl;
+	cout << "My major is Computer Information Systems." << endl;
+	cout << "I love listening to Rock Music and it's sub genres." << endl;
+	cout << "I was born on December 16, 2003.";
+
+	return 0;
+}
+
+/*
+* My name is Sujan Tamang.
+*My major is Computer Information Systems.
+*I love listening to Rock Music and it's sub genres.
+*I was born on December 16, 2003.
+*C:\Users\kimsu\source\repos\WhoAmI.cpp\x64\Debug\WhoAmI.cpp.exe (process 17120) exited with code 0 (0x0).
+*/
